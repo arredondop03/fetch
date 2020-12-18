8a6c25eeb671cb432e9462cc1e53fc4092060a45
Paola Arredondo <arredondop03@gmail.com>
2020-12-18 11:57:24 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
+const App = () => {
+  let [isLoading, setIsLoading] = useState(false);
+  let [data, setData] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetchList();
+    setIsLoading(false);
+  }, []);
+
+  let fetchList = () => {
+    let dataObject = {};
+    axios.get('./list.JSON')
+      .then((response) => {
+        response.data.forEach((item) => {
+          if(dataObject[item.listId] && item.name) {
+            dataObject[item.listId].push(item);
+          } else if(!dataObject[item.listId] && item.name) {
+            dataObject[item.listId] = [item];
+          }
+        });
+        for (const listId in dataObject) {
+          if (dataObject.hasOwnProperty(listId)) {
+            const element = dataObject[listId];
+            sortList(element);
+          }
+        }
+        setData(dataObject);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  let sortList = (list) => {
+    list.sort((a, b) => {
+      let aNumber = a.name.split(' ');
+      let bNumber = b.name.split(' ');
+      let aInt = parseInt(aNumber[1]);
+      let bInt = parseInt(bNumber[1]);
+      return (aInt > bInt) ? 1 : -1;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {Object.keys(data).map(function(key) {
+        return <div key={key}>
+          <h1>List {key}</h1>
+      {data[key].map((item) => <p key={item.id}>{item.id} {item.listId} {item.name}</p>)}
+        </div>
+      })}
     </div>
   );
-}
+};
 
 export default App;
+
+  {/* {
+    isLoading ?
+      <div>Loading...</div> :
+    (
+      <div></div>
+    )
+  } */}
